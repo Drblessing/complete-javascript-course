b1d617420d47ebe0a2020e8ea16e9fc06b4da3d2
Danny Blessing <52829488+Drblessing@users.noreply.github.com>
2021-12-04 18:27:35 -0800

    fundamentals

--- 01-Fundamentals-Part-1/starter/assignments.js
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -1,7 +1,17 @@
-let country = "United States";
-let continent = "North America";
-let population = "330";
+// const country = "United States";
+// const continent = "North America";
+// let population = "330";
 
-console.log(country);
-console.log(continent);
-console.log(population);
+// console.log(country);
+// console.log(continent);
+// console.log(population);
+
+// console.log();
+
+// const isIsland = true;
+// const language = "english";
+
+// console.log(typeof isIsland);
+// console.log(typeof country);
+// console.log(typeof population);
+// console.log(typeof language);
--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,4 +1,4 @@
-let js = "amazing";
+/*let js = "amazing";
 console.log(40 + 8 + 23 - 10);
 
 console.log("Jonas");
@@ -11,3 +11,31 @@ console.log(firstName);
 
 let myFirstJob = "phone rep";
 let myCurrentJob = "programmer";
+*/
+
+// let javascriptIsFun = true;
+// console.log(javascriptIsFun);
+
+// big = 23n;
+
+// console.log(typeof javascriptIsFun);
+// console.log(true);
+// console.log(typeof big);
+// console.log(typeof 3);
+
+// let test = 13;
+// console.log(test);
+
+// console.log(typeof test);
+// test = "test";
+// console.log(test);
+// console.log(typeof test);
+
+// let year;
+// console.log(year);
+// console.log(typeof year);
+
+// console.log(typeof null);
+// //console.log(typeof typeof);
+
+// const birthYear = 1995;
