f93b2e1f9a9b6a67f99ce01952008974f7610b32
Danny Blessing <52829488+Drblessing@users.noreply.github.com>
2021-12-30 16:15:51 -0800

    node.js

--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -1,3 +1,12 @@
 // Remember, we're gonna use strict mode in all scripts now!
 'use strict';
 
+const xyzabc = 23;
+
+if (xyzabc === 23) console.log(23);
+
+const calcAge = birthYear => 2037 - birthYear;
+
+console.log(calcAge(1950));
+
+console.log(1999);
