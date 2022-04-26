a532effd1aab2fa2764f3abadb36a3543f1ce62e
Danny Blessing <52829488+Drblessing@users.noreply.github.com>
2022-04-26 10:22:46 -0400

    forkingfy

--- 18-forkify/starter/dist/index.62406edb.js
+++ 18-forkify/starter/dist/index.62406edb.js
@@ -1,43 +1,33 @@
 const recipeContainer = document.querySelector('.recipe');
-
-const timeout = function (s) {
-  return new Promise(function (_, reject) {
-    setTimeout(function () {
-      reject(new Error(`Request took too long! Timeout after ${s} second`));
-    }, s * 1000);
-  });
+const timeout = function(s) {
+    return new Promise(function(_, reject) {
+        setTimeout(function() {
+            reject(new Error(`Request took too long! Timeout after ${s} second`));
+        }, s * 1000);
+    });
 };
-
 // https://forkify-api.herokuapp.com/v2
-
 ///////////////////////////////////////
-
-const showRecipe = async function () {
-  try {
-    // 1) Loading Recipe
-    const res = await fetch(
-      'https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886'
-      // 'https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bcfcc'
-    );
-    const data = await res.json();
-
-    if (!res.ok) throw new Error(`${data.message} (${res.status})`);
-
-    let { recipe } = data.data;
-    recipe = {
-      id: recipe.id,
-      title: recipe.title,
-      publisher: recipe.publisher,
-      sourceUrl: recipe.source_url,
-      image: recipe.image_url,
-      servings: recipe.servings,
-      cookingTime: recipe.cooking_time,
-      ingredients: recipe.ingredients,
-    };
-    console.log(recipe);
-
-    // 2) Rendering Recipe
-    const markup = `
+const showRecipe = async function() {
+    try {
+        // 1) Loading Recipe
+        const res = await fetch('https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886');
+        const data = await res.json();
+        if (!res.ok) throw new Error(`${data.message} (${res.status})`);
+        let { recipe  } = data.data;
+        recipe = {
+            id: recipe.id,
+            title: recipe.title,
+            publisher: recipe.publisher,
+            sourceUrl: recipe.source_url,
+            image: recipe.image_url,
+            servings: recipe.servings,
+            cookingTime: recipe.cooking_time,
+            ingredients: recipe.ingredients
+        };
+        console.log(recipe);
+        // 2) Rendering Recipe
+        const markup = `
     <figure class="recipe__fig">
     <img src="${recipe.image}" alt=${recipe.title} class="recipe__img" />
     <h1 class="recipe__title">
@@ -132,11 +122,12 @@ const showRecipe = async function () {
     </a>
   </div>
     `;
-    recipeContainer.innerHTML = '';
-    recipeContainer.insertAdjacentHTML('afterbegin', markup);
-  } catch (err) {
-    alert(err);
-  }
+        recipeContainer.innerHTML = '';
+        recipeContainer.insertAdjacentHTML('afterbegin', markup);
+    } catch (err) {
+        alert(err);
+    }
 };
-
 showRecipe();
+
+//# sourceMappingURL=index.62406edb.js.map
