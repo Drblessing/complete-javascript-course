4bca042ae3e05b251ed9be5af621cd1af4bb8703
Danny Blessing <52829488+Drblessing@users.noreply.github.com>
2022-05-22 21:08:36 -0700

    Added new welcome feature

--- 18-forkify/starter/src/js/controller.js
+++ 18-forkify/starter/src/js/controller.js
@@ -112,7 +112,9 @@ const controlAddRecipe = async function (newRecipe) {
     addRecipeView.renderError(err);
   }
 };
-
+const newFeature = function () {
+  console.log('Welcome to the application!');
+};
 const init = function () {
   bookmarksView.addHandlerRender(controlBookmarks);
   recipeView.addHandlerRender(controlRecipes);
@@ -121,5 +123,6 @@ const init = function () {
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
   addRecipeView.addHandlerUpload(controlAddRecipe);
+  newFeature();
 };
 init();
